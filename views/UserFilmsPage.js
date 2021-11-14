107a25000db6ed2d488059ca2e3b749cb49253f2
Joseph Lunine <jel99@drexel.edu>
2021-11-14 16:50:37 -0500

    added call to get the user's film list and display it

--- views/UserFilmsPage.js
+++ views/UserFilmsPage.js
@@ -1,12 +1,86 @@
-import React, { useState, useContext } from 'react';
-import { StyleSheet, Image, Text, TextInput, View, TouchableOpacity, Alert } from 'react-native';
+import React, { useState, useContext, useEffect } from 'react';
+import { StyleSheet, Image, Text, TextInput, View, TouchableOpacity, Alert, Button } from 'react-native';
 import CustomButton from '../components/CustomButton';
-import CryptoJS from 'crypto-js';
 
 import AppContext from "../components/AppContext";
+import FilmCard from '../components/FilmCard';
 
 const UserFilmsPage = (props) => {
 
+    const context = useContext(AppContext);
+
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        getUserFilms();
+    }, [])
+
+    const getUserFilms = () => {
+        try {
+            fetch(context.apiEndpoint + "/films", {
+                method: "GET",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + context.token
+                }
+            }).then(resp => resp.json())
+                .then(results => {
+                    if (results.total > 0) { // if there are any films, continue
+                        console.log(results);
+                        setResults(results.data);
+                    } else { // if no films, show error
+                        setResults([]);
+                        // Alert.alert("No films found! Try adding some to your list.");
+                    }
+                });
+        } catch (e) {
+            console.log(e);
+            console.log("--------------");
+            alert("Error: " + e.message);
+        }
+    }
+
+    return (
+
+        <View style={styles.container}>
+
+            <Button title="Add Film" onPress={() => props.navigation.navigate('Add a Film')} />
+
+                {
+                    results.length > 0 ?
+                        results.map((item) => {
+                            return (
+                                // <Text key={index}>{item.Title}</Text>
+                                <FilmCard key={item._id} film={item} onPress={
+                                    () => {
+                                        props.navigation.navigate('Film Details', { filmId: item._id });
+                                    }
+                                }/>
+                            );
+                        })
+                        :
+                        <Text>Looks like you haven't added any films yet. Try adding some!</Text>
+                }
+
+        </View>
+
+    );
+
 }
+
+// Stylesheet
+const styles = StyleSheet.create({
+    container: {
+      display: 'flex',
+    //   flex: 1,
+      padding: 20,
+      height: '100%',
+      backgroundColor: '#fff'
+    //   alignItems: 'center',
+    //   justifyContent: 'center',
+    },
+
+});
 
 export default UserFilmsPage;
