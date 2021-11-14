671a88546b20a56f126637f7608bfebae2278bbc
Joseph Lunine <jel99@drexel.edu>
2021-11-14 16:49:42 -0500

    added routing and components

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import AppContext from './components/AppContext';
 import LoginPage from './views/LoginPage';
-import HomePage from './views/HomePage';
+// import HomePage from './views/HomePage';
 import UserFilmsPage from './views/UserFilmsPage';
+import FilmPage from './views/filmPages/FilmPage';
+import AddFilmPage from './views/filmPages/AddFilmPage';
+import UpdateFilmPage from './views/filmPages/UpdateFilmPage';
 
 export default function App() {
 
@@ -20,17 +21,22 @@ export default function App() {
     token: jwtToken,
     setToken,
     username: username,
-    setUsername
+    setUsername,
+    apiEndpoint: 'http://10.0.0.38:3001/api/v1',
+    omdbEndpoint: 'https://www.omdbapi.com/?apikey=aa3381be&type=movie'
   };
 
   return (
     <AppContext.Provider value={globalProps}>
 
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator initialRouteName="Login">
           <Stack.Screen name="Login" component={LoginPage} />
-          <Stack.Screen name="Home" component={HomePage} />
-          <Stack.Screen name="Films" component={UserFilmsPage} />
+          {/* <Stack.Screen name="Film Search" component={HomePage} /> */}
+          <Stack.Screen name="Film Library" component={UserFilmsPage} />
+          <Stack.Screen name="Film Details" component={FilmPage} />
+          <Stack.Screen name="Add a Film" component={AddFilmPage} />
+          <Stack.Screen name="Update Film" component={UpdateFilmPage} />
         </Stack.Navigator>
       </NavigationContainer>
 
