a880fc1c17d1ead67e2564f75f4d07c0aa7c770f
Joseph Lunine <jel99@drexel.edu>
2021-11-18 22:40:00 -0500

    made the page nicer, and added new components

--- views/filmPages/FilmPage.js
+++ views/filmPages/FilmPage.js
@@ -1,21 +1,24 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { StyleSheet, Image, Text, TextInput, View, TouchableOpacity, Alert, Button } from 'react-native';
+import { StyleSheet, Text, View, Alert, Button } from 'react-native';
 
 import AppContext from "../../components/AppContext";
+import Rating from '../../components/Rating';
 
-const FilmPage = (props) => {
+const FilmPage = ({navigation, route}) => {
 
     const context = useContext(AppContext);
 
-    const [film, setFilm] = useState();
+    const [film, setFilm] = useState({});
+    const [error, setError] = useState('');
 
     useEffect(() => {
         getFilm();
     }, [])
 
     const getFilm = () => {
+        console.log("Film Id clicked: " + route.params.id);
         try {
-            fetch(context.apiEndpoint + "/films/" + props.filmId, {
+            fetch(context.apiEndpoint + "/films/" + route.params.id, {
                 method: "GET",
                 headers: {
                     'Accept': 'application/json',
@@ -24,11 +27,18 @@ const FilmPage = (props) => {
                 }
             }).then(resp => resp.json())
                 .then(result => {
-                    if (result.status == 200) {
-                        console.log(result);
-                        setFilm(result);
+                    if (result._id) {
+                        // console.log(result);
+                        setFilm({
+                            id: result._id,
+                            title: result.name,
+                            rating: result.rating,
+                            ratingNum: result.rating.split('*').length - 1,
+                            releaseYear: result.releaseYear,
+                            notes: result.notes
+                        });
                     } else { // if no films, show error
-                        Alert.alert("This film doesn't appear to be in your list!");
+                        setError(result.message);
                     }
                 });
         } catch (e) {
@@ -42,9 +52,32 @@ const FilmPage = (props) => {
 
         <View style={styles.container}>
 
-            <Text>{film.name}</Text>
-            <Text>{film.rating}</Text>
-            <Text>{film.releaseYear}</Text>
+            <Button title="Edit" onPress={() => 
+                navigation.navigate('Update your Film', { 
+                        filmName: film.title,
+                        rating: film.rating,
+                        releaseYear: film.releaseYear,
+                        notes: film.notes,
+                        filmId: film.id
+                    })
+                } 
+            />
+
+            <Text style={[styles.title, styles.bold]}>{film.title}</Text>
+            <Text style={[styles.textItem]}>{film.releaseYear ? film.releaseYear : ''}</Text>
+
+            <View style={styles.ratingBox}>
+                <Text style={styles.bold}>Your rating:  </Text>
+                <Rating style={styles.textItem} number={film.ratingNum} />
+            </View>
+            
+            <View>
+                <Text style={styles.bold}>Your notes:</Text>
+                <Text style={styles.textItem}>{film.notes ? film.notes : "No notes yet!"}</Text>
+            </View>
+            
+
+            
 
         </View>
 
@@ -56,14 +89,24 @@ const FilmPage = (props) => {
 const styles = StyleSheet.create({
     container: {
       display: 'flex',
-    //   flex: 1,
       padding: 20,
       height: '100%',
       backgroundColor: '#fff'
-    //   alignItems: 'center',
-    //   justifyContent: 'center',
     },
-
+    bold: {
+        fontWeight: 'bold'
+    },
+    title: {
+        fontSize: 20,
+        marginTop: 20
+    },
+    ratingBox: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    textItem: {
+        marginBottom: 20
+    }
 });
 
 export default FilmPage;
